a7bb9c93c447b96d01721892a1812727776a4f37
Jack Sanders <personal@jacksanders.uk>
2025-01-24 23:43:16 +0000

    feat(api): initial spring boot setup

---
+++ api/src/test/java/uk/jacksanders/letterboxdguessr/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.jacksanders.letterboxdguessr.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
